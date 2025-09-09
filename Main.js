632c6dd7f3ab99620e8dba58ab5759665d3816bd
MTk00911 <mmttkk1531518@gmail.com>
2025-09-09 08:55:20 +0330

    mTk-Codes

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
+let flag = true
+
 $("a#themE").on("click", function () {
-    let flag = true
     if (flag === true) {
         $("header").addClass("lightTheme")
         flag = false
@@ -9,4 +10,19 @@ $("a#themE").on("click", function () {
         $("header").removeClass("lightTheme")
         flag = true
     }
+})
+
+let btNext = document.querySelector("button#btNext")
+let firstAdv = document.querySelector("h1#firstAdv")
+let secondAdv = document.querySelector("h1#secondAdv")
+let btnSecond = document.querySelector("button#btnSecond")
+btNext.addEventListener("click", function () {
+    firstAdv.classList.add("d-nonE")
+    secondAdv.classList.remove("d-nonE")
+
+})
+
+btnSecond.addEventListener("click", function () {
+    firstAdv.classList.remove("d-nonE")
+    secondAdv.classList.add("d-nonE")
 })
